0d8e0e7d071de36b7b89e424afe1028cdcaaa544
phbrgnomo <phbrcc@gmail.com>
2024-10-17 16:29:08 -0300

    a

--- initial_setup/install_brew.sh
+++ initial_setup/install_brew.sh
@@ -15,19 +15,6 @@ else
     install_homebrew
 fi
 
-# Set up Homebrew environment variables for the current session
-if test -d ~/.linuxbrew; then
-    eval "$($(~/.linuxbrew/bin/brew shellenv))"
-elif test -d /home/linuxbrew/.linuxbrew; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
-
-# Ensure Homebrew is added to the shell configuration file
-if ! grep -q 'brew shellenv' ~/.bashrc; then
-    echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
-    echo "Homebrew environment added to .bashrc"
-fi
-
 # Reload the shell configuration
 source ~/.bashrc
 
